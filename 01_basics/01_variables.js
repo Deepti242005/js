451f2347db4144d67e274554195aa59e5f3f2208
Deepti242005 <deeptishetty242005@gmail.com>
2026-01-26 09:37:22 +0000

    file config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId=14453
+let accountEmail="deepti@gmail.com"
+var accountpw="12345"
+accountCity="Jaipur"
+let accountState;
+// accountId=2 not allowed
+console.log(accountId);
+accountEmail="hc@hcgmail.com"
+accountpw="4321"
+accountCity="Bengaluru"
+
+console.table([accountId,accountEmail,accountpw,accountCity,accountState])
