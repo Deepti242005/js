f2d336d13c3e3bdbabcc3d35ca4012fad3c75b20
Deepti242005 <deeptishetty242005@gmail.com>
2026-01-26 10:25:50 +0000

    datatypes and conversion configured

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,9 @@
+let name="deepti"//string
+let age=18 //number
+let isLoggedIn=false
+
+console.log(typeof "deepti");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/03_conversionOp.js
@@ -0,0 +1,15 @@
+let score="33"
+console.log(typeof(score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn="deepti"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+let num=33
+let stringNum=String(num)
+console.log(stringNum)
+console.log(typeof stringNum);
