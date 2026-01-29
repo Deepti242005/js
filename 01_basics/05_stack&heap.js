fb730d2b400dbf3b887ec5e0cb6f0a1cfda9d7e6
Deepti242005 <deeptishetty242005@gmail.com>
2026-01-29 17:38:15 +0000

    stack heap commited

---
+++ 01_basics/05_stack&heap.js
@@ -0,0 +1,15 @@
+let myname="Deepti"
+let name=myname
+name="deepu";
+console.log(myname);
+console.log(name);
+
+let userOne={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+}
+
+let userTwo=userOne
+userTwo.email="deepti@gmail.com"
+console.log(userOne.email)//deepti@gmail.com
+console.log(userTwo.email)//deepti@gmail.com
